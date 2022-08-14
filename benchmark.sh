ce984307393de1f0c65b174a585f1690a8b7e3d5
regr4 <60316183+regr4@users.noreply.github.com>
2022-08-14 18:29:45 +0200

    used rayon and LTO to get down to around 5 seconds per run

---
+++ benchmark.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+RUSTFLAGS="-C target-cpu=native" cargo b --release
+echo >> times.txt
+for i in $(seq 10); do
+    /bin/env time -o times.txt -a ./target/release/uniques-rs
+done
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
-// use std::str::SplitWhitespace;
+use rayon::prelude::*;
 
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    sync::{Mutex, RwLock},
+};
 
 const WORDS: &str = include_str!("words_alpha.txt");
 
 fn main() {
-    let mut words_list: Vec<WordSet> = Vec::new();
-    for word in WORDS
-        .split_whitespace()
+    let mut words_list: Vec<WordSet> = WORDS
+        .par_split_whitespace()
         .filter(|w| w.len() == 5)
-        .map(|s| s.to_uppercase())
-    {
-        let wset = WordSet::from_word(&word);
+        .map(WordSet::from_word)
+        .filter(|w| w.size() == 5)
+        .collect();
 
-        // println!("{}:    {word}", WordSet::from_word(&word).show());
-
-        // ensure only words with 5 unique letters are counted
-        if wset.size() != 5 {
-            continue;
-        }
-        words_list.push(wset);
-    }
-
-    words_list.sort(); // due to the representation, this'll be 'backwards', but it doesn't matter
+    words_list.par_sort(); // due to the representation, this'll be 'backwards', but it doesn't matter
     words_list.dedup();
 
     println!("created word list");
 
     // words_hmap[w] is all the words that
-    // - are disjoint from w
-    // appear later in words_list.
-    let mut words_hmap: HashMap<WordSet, HashSet<WordSet>> = HashMap::new();
-    for (ix, word) in (&words_list).iter().enumerate() {
+    // are disjoint from w and appear later in words_list.
+    let words_hmap: Mutex<HashMap<WordSet, HashSet<WordSet>>> = Mutex::new(HashMap::new());
+    words_list.iter().enumerate().for_each(|(ix, word)| {
         let mut s: HashSet<WordSet> = HashSet::new();
 
         for word2 in &words_list[ix + 1..] {
@@ -40,73 +32,84 @@ fn main() {
             }
         }
 
-        words_hmap.insert(*word, s);
-    }
+        words_hmap.lock().unwrap().insert(*word, s);
+    });
 
+    let words_hmap = words_hmap.into_inner().unwrap();
     println!("initialized hashmap");
 
-    // println!("{:?}", &words_list[0..5]);
-
-    // for word in &words_hmap[&WordSet::from_word("units")] {
-    // println!("{}", &word.show());
-    // }
-
     // find the solutions!
-    let mut solutions: Vec<Solution> = Vec::new();
-    for (word, others) in words_hmap.iter() {
-        for word2 in others {
-            for word3 in &words_hmap[word2] {
+    let solutions: Mutex<Vec<Solution>> = Mutex::new(Vec::new());
+    words_hmap.par_iter().for_each(|(word, others)| {
+        others.par_iter().for_each(|word2| {
+            words_hmap[word2].par_iter().for_each(|word3| {
                 if word.intersection(*word3) != WordSet::empty() {
-                    continue;
+                    return;
                 }
 
-                for word4 in &words_hmap[word3] {
+                words_hmap[word3].par_iter().for_each(|word4| {
                     if word.intersection(*word4) != WordSet::empty()
                         || word2.intersection(*word4) != WordSet::empty()
                     {
-                        continue;
+                        return;
                     }
 
-                    for word5 in &words_hmap[word4] {
+                    words_hmap[word4].par_iter().for_each(|word5| {
                         if word.intersection(*word5) != WordSet::empty()
                             || word2.intersection(*word5) != WordSet::empty()
                             || word3.intersection(*word5) != WordSet::empty()
                         {
-                            continue;
+                            return;
                         }
                         // we've found our 5-tuple!!!
-                        solutions.push(Solution(*word, *word2, *word3, *word4, *word5));
-                    }
-                }
-            }
-        }
-    }
+                        let mut slns = solutions.lock().unwrap();
+                        slns.push(Solution(*word, *word2, *word3, *word4, *word5));
+                    })
+                })
+            })
+        })
+    });
+
+    let solutions = solutions.into_inner().unwrap();
 
     println!("found all solutions");
 
-    let mut inv: HashMap<WordSet, String> = HashMap::new();
-
-    for (idx, solution) in solutions.iter().enumerate() {
-        println!("solution #{}:", idx + 1);
-
+    let inv: RwLock<HashMap<WordSet, String>> = RwLock::new(HashMap::new());
+    solutions.par_iter().for_each(|solution| {
         let Solution(w1, w2, w3, w4, w5) = *solution;
 
+        let res = Mutex::new("solution:\n".to_string());
+
         for w_n in &[w1, w2, w3, w4, w5] {
-            if let Some(w) = inv.get(w_n) {
-                println!("{}", w);
+            let inv_guard = inv.read().unwrap();
+            if let Some(word) = inv_guard.get(w_n) {
+                let mut res = res.lock().unwrap();
+                res.push_str(word);
+                res.push('\n');
+                drop(inv_guard)
             } else {
+                drop(inv_guard);
                 // find the first word that gives w_n and save it in inv to save time if it shows up again
-                for word in WORDS.split_whitespace().filter(|wd| wd.len() == 5) {
-                    if WordSet::from_word(word) == *w_n {
-                        println!("{}", word);
-                        inv.insert(*w_n, word.to_owned());
-                    }
-                }
+                let found_word = WORDS
+                    .par_split_whitespace()
+                    .filter(|wd| wd.len() == 5)
+                    .find_any(|word| WordSet::from_word(word) == *w_n)
+                    .unwrap();
+
+                let mut res = res.lock().unwrap();
+                res.push_str(found_word);
+                res.push('\n');
+                drop(res);
+
+                inv.write().unwrap().insert(*w_n, found_word.to_owned());
             }
         }
 
-        println!();
-    }
+        let res = res.into_inner().unwrap();
+        println!("{res}");
+    });
+
+    println!("done! found {} solutions", solutions.len());
 }
 
 /// represents the set of letters of a word as a bit field
